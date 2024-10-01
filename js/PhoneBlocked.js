88c0ce6ca73d115e4d3da6067112d7b40d57093f
Kontolistik <151897038+Hexamouse@users.noreply.github.com>
2024-10-01 23:31:31 +0700

    Update PhoneBlocked.js

--- js/PhoneBlocked.js
+++ js/PhoneBlocked.js
@@ -1,15 +1,12 @@
-// Fungsi untuk memeriksa apakah perangkat adalah mobile
 function isMobileDevice() {
     return /Mobi|Android/i.test(navigator.userAgent);
 }
 
-// Fungsi untuk memblokir akses dari perangkat mobile
 function blockMobileDevices() {
     if (isMobileDevice()) {
         alert("Mobile is Blocked. Access via Dekstop!~");
-        // window.location.href = "https://example.com/blocked"; // Ganti dengan URL halaman blokir
+        window.location.href = "https://example.com/blocked";
     }
 }
 
-// Panggil fungsi saat halaman dimuat
 window.onload = blockMobileDevices;
