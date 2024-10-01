4a66da13371207b85dcd9f7afa42c1836d881038
Kontolistik <151897038+Hexamouse@users.noreply.github.com>
2024-10-01 23:30:09 +0700

    Update Footer.php

--- pages/Footer.php
+++ pages/Footer.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Footer Keren</title>
-    <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -21,21 +20,21 @@
             flex: 1;
         }
         .footer {
-            background-color: transparent; /* Menghapus warna latar belakang footer */
-            /* backdrop-filter: blur(10px); Efek blur pada latar belakang */
-            color: white; /* Warna teks footer */
-            padding: 5px 0; /* Padding atas dan bawah */
-            text-align: center; /* Teks rata tengah */
+            background-color: transparent;
+/*             backdrop-filter: blur(10px); */
+            color: white;
+            padding: 5px 0;
+            text-align: center;
             border-top-left-radius: 10px;
             border-top-right-radius: 10px;
-            margin-top: auto; /* Tambahkan ini untuk memastikan footer tetap di bawah */
+            margin-top: auto;
         }
         .footer a {
-            color: white; /* Warna teks link */
-            transition: color 0.3s; /* Tambahkan animasi transisi untuk link */
+            color: white;
+            transition: color 0.3s;
         }
         .footer a:hover {
-            color: #83000B; /* Warna teks link saat hover */
+            color: #83000B;
         }
     </style>
 </head>
@@ -48,9 +47,6 @@
             </div>
         </div>
     </footer>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
