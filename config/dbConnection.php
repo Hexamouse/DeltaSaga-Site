3798eb906805c34f109be5e501c0892966b2ecf5
Kontolistik <151897038+Hexamouse@users.noreply.github.com>
2024-10-01 23:32:50 +0700

    Update dbConnection.php

--- config/dbConnection.php
+++ config/dbConnection.php
@@ -4,10 +4,8 @@ $username = "root";
 $password = "";
 $dbname = "delta_db";
 
-// Membuat koneksi
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Cek koneksi
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
